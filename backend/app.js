4bfd6ffc5b7580ad4de6573336702966eba62c10
Gabykap <gabriacos@gmail.com>
2023-06-24 14:17:55 -0300

    Se empieza nuevamente con el bakend

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
+
+
+const app = express();
+
+require('ejs');
+app.set('view engine', 'ejs');
+
+//la carpeta public 
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+//variables de entorno
+dotenv.config({path: './env/.env'})
+
+app.get('/',(req,res)=>{
+    res.send('Hola mundo')
+});
+
+
+const port = process.env.port || 3000;
+
+app.listen(port, ()=>{
+    console.log(`servidor corriendo en http://localhost:${port}`)
+});
