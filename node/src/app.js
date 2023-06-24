1e4f8e92c4b3b6b062efdeef61f098307d6a0aff
Gabykap <gabriacos@gmail.com>
2023-06-23 21:05:36 -0300

    Se realizó la estructuración del proyecto.. a comenzar de nuevo

--- node/src/app.js
+++ node/src/app.js
@@ -12,18 +12,28 @@ app.engine('.hbs',exphbs({
     defaultLayaout: 'main',
     layaoutsDir: path.join(app.get('views'),"layaouts"),
     partialsDir: path.join(app.get('views'),'partials'),
-    extname: ''
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
 }));
-
+app.set('view','engine','.hbs');
 const app = express();
 const port = 3000;
 app.use(morgan('dev'));
 app.use(cors());
 app.use(helmet());
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
 
 app.use(require('./routes/router'));
 
+app.use(require('./routes/auth'));
+app.use('links',require('./routes/links'));
 
+app.use((req, res, next) =>{
+    next();
+})
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port,()=> console.log(`Servidor corriendo en http://localhost:${port}`));
