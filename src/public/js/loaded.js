f663be93d59c5d275c4cd159b6735b261ba130a4
Gabriel Acosta <gabriacos@gmail.com>
2023-09-23 12:09:48 -0300

    feat: Cambio de Header y sidebard - Se modificó el header totalmente, agregando el color negro e items de home, informes, personas, usuarios - Se agregó contenido al archivo loaded.js para que al ingresar con una pantalla menor a 850px el aside se elimine - Se modificó el footer a color negro - Las vistas son resposives.

--- src/public/js/loaded.js
+++ src/public/js/loaded.js
@@ -2,3 +2,36 @@ window.onload = function(){
     $('#onload').fadeOut();
     $('body').removeClass('hidden')
 }
+
+
+//función para ocultar el aside
+
+window.addEventListener('DOMContentLoaded', () => {
+    const aside = document.getElementById('aside');
+
+    const adjustLayout = () => {
+        try {
+            if (window.innerWidth <= 876) {
+                aside.style.display = 'none';
+                let canvasContent = document.querySelector('.graficos.col-9');
+                let clases = canvasContent.classList;
+                clases.remove('col-9');
+                clases.add('col-12');
+            } else {
+                aside.style.display = 'block';
+                let canvasContent = document.querySelector('.graficos.col-9');
+                let clases = canvasContent.classList;
+                clases.remove('col-12');
+                clases.add('col-9');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    
+
+    adjustLayout();
+
+    window.addEventListener('resize', adjustLayout);
+});
+
